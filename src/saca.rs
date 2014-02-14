39118c46fd2488e561a10384fae23f3f69857da5
Dzmitry Malyshau <kvarkus@gmail.com>
2014-02-13 20:09:54 -0500

    A skeleton for SACA is implemented.

--- src/saca.rs
+++ src/saca.rs
@@ -17,6 +17,130 @@ use compress::bwt;
 pub type Symbol = u8;
 pub type Suffix = uint;
 
+static EMPTY: Suffix = 0x7FFFFFFF;
+
+
+fn get_buckets(input: &[Symbol], buckets: &mut [uint], end: bool) {
+	for buck in buckets.mut_iter() {
+		*buck = 0;
+	}
+	for sym in input.iter() {
+		buckets[*sym] += 1;
+	}
+	let mut sum = 0u;
+	for buck in buckets.mut_iter() {
+		sum += *buck;
+		*buck = if end {sum} else {sum - *buck};
+	}
+}
+
+fn put_substr0(_suffixes: &mut [Suffix], input: &[Symbol], buckets: &mut [uint]) {
+	// Find the end of each bucket.
+	get_buckets(input, buckets, true);
+
+	//TODO
+}
+
+fn induce_l0(_suffixes: &mut [Suffix], _input: &[Symbol], _buckets: &mut [uint], _clean: bool) {
+	//TODO
+}
+
+fn induce_s0(_suffixes: &mut [Suffix], _input: &[Symbol], _buckets: &mut [uint], _clean: bool) {
+	//TODO
+}
+
+fn gather_lms(suffixes: &mut [Suffix], input: &[Suffix]) {
+	//TODO
+	
+	for suf in suffixes.mut_iter() {
+		*suf = input[*suf];
+	}
+}
+
+fn put_suffix0(suffixes: &mut [Suffix], n1: uint, input: &[Symbol], buckets: &mut [uint]) {
+	// Find the end of each bucket.
+	get_buckets(input, buckets, true);
+
+	for i in range(0,n1).rev() {
+		let p = suffixes[i];
+		suffixes[i] = 0;
+		let sym = input[p];
+		buckets[sym] -= 1;
+		suffixes[buckets[sym]] = p;
+	}
+
+	// Set the single sentinel suffix.
+	suffixes[0] = input.len()-1;
+}
+
+fn name_substr(_suffixes: &mut [Suffix], _input: &[Symbol], _new_input: &[Suffix], _level: uint) -> uint {
+	0 //TODO
+}
+
+fn saca_kn(input: &[Suffix], suffixes: &mut [Suffix], _level: uint) {
+	//TODO
+
+	let n1 = 0u;
+
+	assert!(n1+n1 <= input.len());
+	let (sa_new, input_new) = suffixes.mut_split_at(n1);
+	//TODO
+
+	// Stage 3: induce SA(S) from SA(S1).
+	gather_lms(sa_new, input_new);
+	for s in input_new.mut_iter() {
+		*s = EMPTY;
+	}
+
+	//TODO
+}
+
+fn saca_k0(input: &[Symbol], K: uint, suffixes: &mut [Suffix]) {
+	let mut buckets = vec::from_elem(K, 0u);
+
+	// Stage 1: reduce the problem by at least 1/2.
+	put_substr0(suffixes, input, buckets.as_mut_slice());
+	induce_l0(suffixes, input, buckets.as_mut_slice(), false);
+	induce_s0(suffixes, input, buckets.as_mut_slice(), false);
+
+	// Now, all the LMS-substrings are sorted and stored sparsely in SA.
+	// Compact all the sorted substrings into the first n1 items of SA.
+	let mut n1 = 0u;
+	for i in range(0, suffixes.len()) {
+		if suffixes[i]>0 {
+			suffixes[n1] = suffixes[i];
+			n1 += 1;
+		}
+	}
+	
+	// Stage 2: solve the reduced problem.
+	{
+		assert!(n1+n1 <= input.len());
+		let (sa_new, input_new) = suffixes.mut_split_at(n1);
+		let num_names = name_substr(sa_new, input, input_new, 0);
+
+		if num_names < n1 {
+			// Recurse if names are not yet unique.
+			saca_kn(input_new, sa_new, 1);
+		}else {
+			// Get the suffix array of s1 directly.
+			for (i,&sym) in input_new.iter().enumerate() {
+				sa_new[sym] = i;
+			}
+		}
+
+		// Stage 3: induce SA(S) from SA(S1).
+		gather_lms(sa_new, input_new);
+		for s in input_new.mut_iter() {
+			*s = 0;
+		}
+	}
+
+	put_suffix0(suffixes, n1, input, buckets.as_mut_slice());
+	induce_l0(suffixes, input, buckets.as_mut_slice(), true);
+	induce_s0(suffixes, input, buckets.as_mut_slice(), true);
+}
+
 
 /// main entry point for SAC
 pub fn construct_suffix_array(input: &[Symbol], suffixes: &mut [Suffix]) {
@@ -25,11 +149,15 @@ pub fn construct_suffix_array(input: &[Symbol], suffixes: &mut [Suffix]) {
 		*p = i as Suffix;
 	}
 
-	suffixes.sort_by(|&a,&b| {
-		iter::order::cmp(
-			input.slice_from(a).iter(),
-			input.slice_from(b).iter())
-	});
+	if true {
+		saca_k0(input, 0x100, suffixes);
+	}else {
+		suffixes.sort_by(|&a,&b| {
+			iter::order::cmp(
+				input.slice_from(a).iter(),
+				input.slice_from(b).iter())
+		});
+	}
 
 	debug!("construct suf: {:?}", suffixes);
 }
@@ -137,11 +265,16 @@ impl<'a> Iterator<Symbol> for InverseIterator<'a> {
 
 #[cfg(test)]
 pub mod test {
-	use std::vec;
-	use super::{Suffix, Symbol};
+	use std::{mem, vec};
+	use super::{EMPTY, Suffix, Symbol};
 
 	#[test]
-	fn some() {
+	fn consts() {
+		assert_eq!(EMPTY, 1<<(mem::size_of::<Suffix>()*8-1));
+	}
+
+	#[test]
+	fn abracadabra() {
 		let input = bytes!("abracadabra");
 		let mut suf = vec::from_elem(input.len(), 0 as Suffix);
 		super::construct_suffix_array(input, suf);
