1d17563513b5f0611d5bd00b1000cb19899946ce
Dzmitry Malyshau <kvarkus@gmail.com>
2014-02-19 18:02:35 -0500

    Using BWT iterators from rust-compress now.

--- src/saca.rs
+++ src/saca.rs
@@ -12,7 +12,6 @@ https://code.google.com/p/ge-nong/
 */
 
 use std::{iter, vec};
-use compress::bwt;
 
 pub type Symbol = u8;
 pub type Suffix = u32;
@@ -381,113 +380,23 @@ impl Constructor {
 }
 
 
-/// An iterator over BWT output
-pub struct LastColumnIterator<'a> {
-	priv input		: &'a [Symbol],
-	priv suf_iter	: iter::Enumerate<vec::Items<'a,Suffix>>,
-	priv origin		: Option<uint>,
-}
-
-impl<'a> LastColumnIterator<'a> {
-	/// create a new BWT iterator from the suffix array
-	pub fn new(input: &'a [Symbol], suffixes: &'a [Suffix]) -> LastColumnIterator<'a> {
-		LastColumnIterator {
-			input: input,
-			suf_iter: suffixes.iter().enumerate(),
-			origin: None,
-		}
-	}
-
-	/// return the index of the original string
-	pub fn get_origin(&self) -> uint {
-		self.origin.unwrap()
-	}
-}
-
-impl<'a> Iterator<Symbol> for LastColumnIterator<'a> {
-	fn next(&mut self) -> Option<Symbol> {
-		self.suf_iter.next().map(|(i,&p)| {
-			if p == 0 {
-				assert!( self.origin.is_none() );
-				self.origin = Some(i);
-				*self.input.last().unwrap()
-			}else {
-				self.input[p-1]
-			}
-		})
-	}
-}
-
-
-/// An iterator over inverse BWT
-pub struct InverseIterator<'a> {
-	priv input		: &'a [Symbol],
-	priv suffixes	: &'a [Suffix],
-	priv origin		: uint,
-	priv current	: Suffix,
-}
-
-impl<'a> InverseIterator<'a> {
-	/// create a new inverse BWT iterator
-	pub fn new(input: &'a [Symbol], origin: uint, suffixes: &'a mut [Suffix]) -> InverseIterator<'a> {
-		assert_eq!(input.len(), suffixes.len())
-		debug!("decode origin={}, input: {:?}", origin, input)
-
-		let mut radix = bwt::Radix::new();
-		radix.gather(input);
-		radix.accumulate();
-
-		suffixes[radix.place(input[origin])] = 0;
-		for (i,&ch) in input.slice_to(origin).iter().enumerate() {
-			suffixes[radix.place(ch)] = (i+1) as Suffix;
-		}
-		for (i,&ch) in input.slice_from(origin+1).iter().enumerate() {
-			suffixes[radix.place(ch)] = (origin+2+i) as Suffix;
-		}
-		//suffixes[-1] = origin;
-		debug!("decode table: {:?}", suffixes)
-
-		InverseIterator {
-			input: input,
-			suffixes: suffixes,
-			origin: origin,
-			current: origin as Suffix,
-		}
-	}
-}
-
-impl<'a> Iterator<Symbol> for InverseIterator<'a> {
-	fn next(&mut self) -> Option<Symbol> {
-		if self.current == -1 {
-			None
-		}else {
-			self.current = self.suffixes[self.current] - 1;
-			debug!("\tjumped to {}", self.current);
-			let p = if self.current!=-1 {
-				self.current
-			}else {
-				self.origin as Suffix
-			};
-			Some(self.input[p])
-		}
-	}
-}
-
 #[cfg(test)]
 pub mod test {
+	use compress::bwt;
+
 	fn some_detail(input: &[super::Symbol], suf_expected: &[super::Suffix], origin_expected: uint, out_expected: &[super::Symbol]) {
 		let mut con = super::Constructor::new(input.len());
 		let (output, origin) = {
 			let suf = con.compute(input);
 			assert_eq!(suf.as_slice(), suf_expected);
-			let mut iter = super::LastColumnIterator::new(input,suf);
+			let mut iter = bwt::TransformIterator::new(input,suf);
 			let out = iter.by_ref().take(input.len()).to_owned_vec();
 			(out, iter.get_origin())
 		};
 		assert_eq!(origin, origin_expected);
 		assert_eq!(output.as_slice(), out_expected);
 		let suf = con.reuse().mut_slice_to(input.len());
-		let decoded = super::InverseIterator::new(output, origin, suf).to_owned_vec();
+		let decoded = bwt::decode(output, origin, suf).to_owned_vec();
 		assert_eq!(input.as_slice(), decoded.as_slice());
 	}
 
