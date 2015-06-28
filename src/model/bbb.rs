a80b298d6676ce6aa025b3d1c7f459aed63d56e7
Dzmitry Malyshau <kvarkus@gmail.com>
2015-06-28 14:09:01 -0400

    Basic bbb non-adaptive implementation with a test

--- src/model/bbb.rs
+++ src/model/bbb.rs
@@ -22,16 +22,32 @@ const LOG_DEFAULT   : u32 = 1<<FIXED_BASE;
 
 /// Coding model for BWT-DC output
 pub struct Model {
-    avg_log : [u32; 0x100],   //fixed-point
-    prob    : [[ari::apm::Bit; 24]; LOG_LIMIT],
+    next_prob: ari::apm::Bit,
+    avg_log: [u32; 0x100],   //fixed-point
+    prob: [[ari::apm::Bit; 24]; LOG_LIMIT],
 }
 
 impl Model {
     /// Create a new model
     pub fn new() -> Model {
         Model {
-            avg_log : [LOG_DEFAULT; 0x100],
-            prob    : [[ari::apm::Bit::new_equal(); 24]; LOG_LIMIT],
+            next_prob: ari::apm::Bit::new_equal(),
+            avg_log: [LOG_DEFAULT; 0x100],
+            prob: [[ari::apm::Bit::new_equal(); 24]; LOG_LIMIT],
+        }
+    }
+
+    fn update(&mut self, bit: u8) {
+        //TODO
+    }
+
+    fn predict(&self) -> ari::apm::Bit {
+        if self.next_prob.predict() {
+            self.next_prob.clone()
+        }else {
+            // weird hack by MM, TODO: investigate
+            let flat = self.next_prob.to_flat() + 1;
+            ari::apm::Bit::from_flat(flat)
         }
     }
 }
@@ -48,11 +64,25 @@ impl super::Model<Symbol, Context> for Model {
         }
     }
 
-    fn encode<W: io::Write>(&mut self, _sym: Symbol, _ctx: &Context, _eh: &mut ari::Encoder<W>) {
-        //TODO
+    fn encode<W: io::Write>(&mut self, sym: Symbol, _ctx: &Context,
+              eh: &mut ari::Encoder<W>) -> io::Result<()> {
+        for i in (0..8).rev() {
+            let bit = (sym >> i) & 1;
+            try!(eh.encode(bit != 0, &self.predict()));
+            self.update(bit);
+        }
+        Ok(())
     }
 
-    fn decode<R: io::Read>(&mut self, _ctx: &Context, _dh: &mut ari::Decoder<R>) -> Symbol {
-        0 //TODO
+    fn decode<R: io::Read>(&mut self, _ctx: &Context, dh: &mut ari::Decoder<R>)
+              -> io::Result<Symbol> {
+        let mut sym = 0 as Symbol;
+        for i in (0..8).rev() {
+            let bit_b = try!(dh.decode( &self.predict() ));
+            let bit = if bit_b {1} else {0};
+            self.update(bit);
+            sym |= bit << i;
+        }
+        Ok(sym)
     }
 }
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -85,9 +85,9 @@ pub mod test {
     use compress::bwt::dc;
     use compress::entropy::ari;
     use super::{Distance, DistanceModel};
-    use super::Symbol;
+    use super::{RawModel, Symbol, SymContext};
 
-    fn roundtrip<M: DistanceModel>(m: &mut M, input: &[(Distance, dc::Context)]) {
+    fn roundtrip_dc<M: DistanceModel>(m: &mut M, input: &[(Distance, dc::Context)]) {
         let mut eh = ari::Encoder::new(Vec::new());
         m.reset();
         for &(dist, ref ctx) in input.iter() {
@@ -105,7 +105,25 @@ pub mod test {
         }
     }
 
-    fn gen_data(size: usize, max_dist: Distance) -> Vec<(Distance, dc::Context)> {
+    fn roundtrip_raw<M: RawModel>(mut m: M, input: &[(Symbol, SymContext)]) {
+        let mut eh = ari::Encoder::new(Vec::new());
+        m.reset();
+        for &(sym, ref ctx) in input.iter() {
+            debug!("Encode: {}", sym);
+            m.encode(sym, ctx, &mut eh).unwrap();
+        }
+        let (mem, err) = eh.finish();
+        err.unwrap();
+        m.reset();
+        let mut dh = ari::Decoder::new(io::BufReader::new(io::Cursor::new(&mem[..])));
+        for &(sym, ref ctx) in input.iter() {
+            let sym2 = m.decode(ctx, &mut dh).unwrap();
+            debug!("Actual: {}, Decoded: {}", sym, sym2);
+            assert_eq!(sym, sym2);
+        }
+    }
+
+    fn gen_data_dc(size: usize, max_dist: Distance) -> Vec<(Distance, dc::Context)> {
         let mut rng = StdRng::new().unwrap();
         (0..size).map(|_| {
             let sym: Symbol = rng.gen();
@@ -114,33 +132,47 @@ pub mod test {
         }).collect()
     }
 
-    fn roundtrips<M: DistanceModel>(mut m: M) {
-        roundtrip(&mut m, &[
+    fn gen_data_raw(size: usize) -> Vec<(Symbol, SymContext)> {
+        let mut rng = StdRng::new().unwrap();
+        (0..size).map(|_| {
+            let sym: Symbol = rng.gen();
+            (sym, ())
+        }).collect()
+    }
+
+    fn roundtrips_dc<M: DistanceModel>(mut m: M) {
+        roundtrip_dc(&mut m, &[
             (1, dc::Context::new(1,1,5)),
             (2, dc::Context::new(2,2,5)),
             (3, dc::Context::new(3,3,5)),
             (4, dc::Context::new(4,4,5))
             ]);
-        roundtrip(&mut m, &gen_data(1000,200));
+        roundtrip_dc(&mut m, &gen_data_dc(1000,200));
+    }
+
+    #[test]
+    fn roundtrip_bbb() {
+        let input = gen_data_raw(1000);
+        roundtrip_raw(super::bbb::Model::new(), &input);
     }
     
     #[test]
     fn roundtrips_dark() {
-        roundtrips(super::dark::Model::new());
+        roundtrips_dc(super::dark::Model::new());
     }
 
     #[test]
     fn roundtrips_exp() {
-        roundtrips(super::exp::Model::new());
+        roundtrips_dc(super::exp::Model::new());
     }
 
     #[test]
     fn roundtrips_simple() {
-        roundtrips(super::simple::Model::new());
+        roundtrips_dc(super::simple::Model::new());
     }
 
     #[test]
     fn roundtrips_ybs() {
-        roundtrips(super::ybs::Model::new());
+        roundtrips_dc(super::ybs::Model::new());
     }
 }
